b57803fc1c0be25965ed806178ec9a3da5a7f4b8
nafizurr <nafizurr@buffalo.edu>
2025-06-29 00:09:24 -0400

    feat: initialize frontend with React and Vite setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application layout and components.
    - Implemented basic App component structure in App.jsx.
    - Added React logo SVG in assets directory.
    - Developed MovieCard component for displaying movie details and favorite functionality.
    - Set up global styles in index.css for consistent styling across the application.
    - Configured main entry point in main.jsx to render the App component.
    - Created Vite configuration file for project setup.

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+function MovieCard({movie}) { //info about the movie
+
+    function onFavoriteClick() {
+        alert("clicked")
+    }
+
+    return <div className="movie-card"> {/* in my movie card, i have the image, the overlay and the button that will blur out and show the heart, all inside the parent div, on child being the poster and the other being info */}
+        <div className="movie-poster">
+            <img src = {movie.url} alt = {movie.title}/>
+            <div className = "movie-overlay">
+                <button className="favorite-btn" onclick = {onFavoriteClick}>
+                    ❤️
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3> {/* Using Braces as a variable */}
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
+}
+
+export default MovieCard
